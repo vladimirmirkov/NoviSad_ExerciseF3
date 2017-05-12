5068c790a63480ce36d84ce7ff3abf799f0d63b5
somename <someemail>
2017-05-12 20:45:50 +0200

    besouro automatic message

--- src/Explorer.java
+++ src/Explorer.java
@@ -1,4 +1,73 @@
 
 public class Explorer {
 
+	private int x = 0;
+	private int y = 0;
+	private String direction = "N";
+	private int[] grid;
+	
+	public Explorer()
+	{
+		
+	}
+	
+	public Explorer(int x, int y, String obstacles)
+	{
+		this.setX(x);
+		this.setY(y);
+		
+	}
+	
+	public void moveForth()
+	{
+		if(direction == "N" && y < grid[1])
+			y++;
+		else if(direction == "N" && y == grid[1])
+			y = 0;
+		if(direction == "E" && x < grid[0])
+			x++;
+		else if(direction == "E" && x == grid[0])
+			x = 0;
+		if(direction == "S" && y > 0)
+			y--;
+		else if(direction == "S" && y == 0)
+			y = grid[1];
+		if(direction == "W" && x > 0)
+			x--;
+		else if(direction == "W" && x == 0)
+			x = grid[0];
+	}
+	
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+
+	public int[] getGrid() {
+		return grid;
+	}
+
+	public void setGrid(int[] grid) {
+		this.grid = grid;
+	}
 }
--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,11 @@
 // Student ID:
 
 public class PlanetExplorer {
+	private int grid_x;
+	private int grid_y;
+	private String Obstacles;
+	Explorer exp = new Explorer(grid_x, grid_y, Obstacles);
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -12,6 +17,9 @@ public class PlanetExplorer {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
 		 
 	 */
+		grid_x = x;
+		grid_y = y;
+		this.Obstacles = obstacles;
 	}
 	
 	public String executeCommand(String command){
@@ -25,6 +33,11 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
+		
+		if(command == "f")
+		{
+			exp.moveForth();
+		}
 		
 		return null;
 	}
